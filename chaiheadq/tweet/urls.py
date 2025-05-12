86a7abc996daf51031108fb5414c79df2d419414
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2025-05-12 17:35:21 +0600

    Delete chaiheadq directory

--- chaiheadq/tweet/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from . import views
-urlpatterns = [
-    path('', views.tweet_list, name = 'tweet_list'),
-    path('tweet_create/', views.tweet_create, name = 'tweet_create'),
-    path('<int:tweet_id>/edit/', views.tweet_edit, name = 'tweet_edit'),
-    path('<int:tweet_id>/delete/', views.tweet_delete, name = 'tweet_delete'),
-]
